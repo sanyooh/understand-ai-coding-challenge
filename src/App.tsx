c9fffc700d441fd534aa817520b667783e0b1288
sandraoldenburg <sandra@spread.ai>
2023-05-17 22:37:42 +0200

    Plotting frames on image selected by group

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Box, FormControl, InputLabel, MenuItem, Select } from '@mui/material';
+import useAnnotations from 'hooks/useAnnotations';
+import _ from 'lodash';
+import image from 'mocks/002_RgbMaster_69426.jpg';
+import React, { useMemo, useState } from 'react';
+
+const floatToPixel = (float: number) => `${Math.floor(float)}px`;
+
+const frameId = '002_RgbMaster_69426';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [selectedGroup, setSelectedGroup] = useState('');
+    const data = useAnnotations();
+    const groups = useMemo(() => _.groupBy(data, 'label'), [data]);
+    const frames = useMemo(() => {
+        if (!groups || !selectedGroup) {
+            return [];
+        }
+        const framesGroupedByImage = _.groupBy(
+            groups[selectedGroup],
+            'frameId'
+        );
+        return framesGroupedByImage[frameId];
+    }, [groups, selectedGroup]);
+
+    return (
+        <Box p="20px">
+            <h2>Understand.ai Coding Challenge</h2>
+            <FormControl sx={{ minWidth: 400, margin: '10px 0' }}>
+                <InputLabel id="demo-simple-select-label">
+                    Annotation Label
+                </InputLabel>
+                <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={selectedGroup}
+                    label="Annotation Label"
+                    onChange={event => setSelectedGroup(event.target.value)}
+                >
+                    {Object.keys(groups).map(groupKey => {
+                        return <MenuItem value={groupKey}>{groupKey}</MenuItem>;
+                    })}
+                </Select>
+            </FormControl>
+            <Box gap="10px"></Box>
+            <Box position="relative">
+                {frames &&
+                    frames.map(item => {
+                        return (
+                            <Box
+                                position="absolute"
+                                left={floatToPixel(item.x)}
+                                top={floatToPixel(item.y)}
+                                width={floatToPixel(item.width)}
+                                height={floatToPixel(item.height)}
+                                border="1px solid red"
+                                color="red"
+                            />
+                        );
+                    })}
+                <img src={image} alt="source" />
+            </Box>
+        </Box>
+    );
 }
 
 export default App;
---
+++ src/hooks/useAnnotations.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+import data from 'mocks/annotations.json';
+
+interface Frame {
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+    label: string;
+    frameId: string;
+}
+
+type Frames = Frame[];
+
+interface AnnotationsResponse {
+    result: Frames;
+}
+
+const useAnnotations = (): Frames => {
+    const [annotations, setAnnotations] = useState<Frames>([]);
+
+    useEffect(() => {
+        setAnnotations((data as AnnotationsResponse).result);
+    }, []);
+
+    return annotations;
+};
+
+export default useAnnotations;
